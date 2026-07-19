9d68ffd26a094e0f9f89189e017d2405d31c60eb
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-07-19 11:35:08 +0530

    implemented login and signup form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,15 @@ import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
+import Login from "./components/Login/Login";
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
+      {showLogin ? <Login setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
